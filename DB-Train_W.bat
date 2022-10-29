2dc66a8c2fc7d0439bb40300eee5fbf2cdf252f6
Arkitecc <50303615+Arkitecc@users.noreply.github.com>
2022-10-29 14:58:56 -0400

    Cont. last push

--- DB-Train_W.bat
+++ DB-Train_W.bat
@@ -4,4 +4,4 @@ SET project_name=shinobu
 SET batch_size=394
 SET class_word=person
 SET reg_data_root=%USERPROFILE%/Dreambooth-SD-optimized/reg_images/
-python "%USERPROFILE%\Dreambooth-SD-optimized\main.py" "--base" "%USERPROFILE%\Dreambooth-SD-optimized\configs\stable-diffusion\v1-finetune_unfrozen.yaml" "-t" "--actual_resume" "%USERPROFILE%\Dreambooth-SD-optimized\wd-1d3.ckpt" "--reg_data_root" "%reg_data_root%" "-n" "%project_name%" "--gpus" "0," "--data_root" "%USERPROFILE%\Dreambooth-SD-optimized\training_images" "--batch_size" "%batch_size%" "--class_word" "%class_word%"
+python "%USERPROFILE%\Dreambooth-SD-optimized\main.py" "--base" "%USERPROFILE%\Dreambooth-SD-optimized\configs\stable-diffusion\v1-finetune_unfrozen.yaml" "-t" "--actual_resume" "%USERPROFILE%\Dreambooth-SD-optimized\model.ckpt" "--reg_data_root" "%reg_data_root%" "-n" "%project_name%" "--gpus" "0," "--data_root" "%USERPROFILE%\Dreambooth-SD-optimized\training_images" "--batch_size" "%batch_size%" "--class_word" "%class_word%"
